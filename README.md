6127332cdb6216c38d5fc152974e912472b7a0e5
Irene Mateo Herrero <irenemherrero@gmail.com>
2023-01-03 14:05:49 +0100

    feat: finished oauth login with google and sessions

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # login-exercise
 
+OAuth2 Login exercise with Passport Google Strategy. Extra security features. Sessions. 
+## setup
 npm start
-https://localhost:3000
 
-secure with https with certificate and key from OpenSSL
-helmet: provides middlewares to secure our server from different hacker attacks. For example, it removes some info from headers sent by the server to the client.
+go to https://localhost:3000
+
+## Info
+
+- Secured https endpoints with certificate and key from OpenSSL
+- Helmet: provides middlewares to secure server from different hacker attacks. For example, it removes some info from headers sent by the server to the client.
+- Client side session
+- Comments to describe the flow and extra possible features
+
+## Description
+
+![alt text](schema.png)
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const path = require('path')
 const express = require('express')
 const https = require('https')
 const fs = require('fs')
+
 const helmet = require('helmet')
 const passport = require('passport')
-// const cookieSession = require('cookie-session')
-const { Strategy } = require('passport-google-oauth2')
+const cookieSession = require('cookie-session')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 require('dotenv').config()
 
@@ -25,37 +26,47 @@ const AUTH_OPTIONS = {
 }
 
 const verifyCallback = (accessToken, refreshToken, profile, done) => {
-	console.log('accessToken', accessToken)
 	done(null, profile)
 }
 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+passport.use(new GoogleStrategy(AUTH_OPTIONS, verifyCallback))
 
-// Save session to the cookie (encriptar)
-// passport.serializeUser((user, done) => {
-// 	done(null, user)
-// })
+// 3. middleware that encripts the cookie after successful login following cookieSession config (below, line 56)
+// sends the resulting encripted cookie to the browser -> cookie contains encripted user data
+// saves the cookie in passport to check if the user exists when it receives a cookie from the browser
+passport.serializeUser((user, done) => {
+	done(null, user.id)
+})
 
-// Read session from cookie (desencriptar)
-// passport.deserializeUser((obj, done) => {
-// 	done(null, obj)
-// })
+// 4. middleware that translates the cookie received from the browser to get the user data,
+// Checkes that the session for this user exists (from saved ones in passport)
+// Could also add some data from user already available in a database, see commented code below.
+// If session is ok, req param in following routes will have req.user set, with user data.
+passport.deserializeUser((id, done) => {
+	// User.findById(id).then(user => {
+	// 	done(null, user)
+	// })
+	done(null, id)
+})
 
 const app = express()
 
-app.use(helmet()) // se pone encima de todos los endpoints para asegurarlos
+app.use(helmet()) // it is set on top of all middlewares below to secure them
 
-// app.use(cookieSession({
-// 	name: 'session',
-// 	maxAge: 24 * 60 * 60 * 1000,
-// 	keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
-// }))
+app.use(cookieSession({
+	name: 'session',
+	maxAge: 24 * 60 * 60 * 1000,
+	keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+	// keys: sirve para firmar la cookie, otra capa de seguridad. 
+	// Cuando nos venga una cookie del browser, sabremos si es nuestra porque se ha encriptado con esta key
+	// esto se hace con las client side sessions, o sea, las que se guardan en las cookies del navegador
+}))
 
 app.use(passport.initialize())
-// app.use(passport.session())
+app.use(passport.session()) 
 
 const checkLoggedIn = (req, res, next) => {
-	const isLoggedIn = true
+	const isLoggedIn = req.isAuthenticated() && req.user
 	if (!isLoggedIn) {
 		return res.status(401).json({
 			error: 'You must log in!'
@@ -64,27 +75,20 @@ const checkLoggedIn = (req, res, next) => {
 	next()
 }
 
-// 1. endpoint para redirigir a la página de login de google
+// 1. login endpoint that redirects to google login page
 app.get('/auth/google', 
 	passport.authenticate('google', {
 		scope: ['email']
 	})
 )
 
-// 2. endpoint al que se va a devolver la respuesta de google cuando termine el proceso de auth
+// 2. callback endpoint that receives the response from google after login.
 app.get('/auth/google/callback', 
-	passport.authenticate('google', {
-		failureRedirect: '/failure',
-		successRedirect: '/',
-	}, 
-	(req, res) => {
-		console.log('Google called us back!')
-	})
-)
+	passport.authenticate('google', { failureRedirect: '/failure', successReturnToOrRedirect: '/successLogin', }))
 
-// endpoint de logout
 app.get('/auth/logout', (req, res) => {
 	req.logout()
+	return res.redirect('/')
 })
 
 app.get('/secret', checkLoggedIn, (req, res) => {
@@ -95,11 +99,15 @@ app.get('/failure', (req, res) => {
 	res.send('Failed sign in')
 })
 
+app.get('/successLogin', (req, res) => {
+	res.send('You logged in successfully')
+})
+
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-// versión no segura sin https
+// Non secure alterantive to create the server without https.
 // app.listen(PORT, () => {
 // 	console.log(`Listening on port ${PORT}`)
 // })
